033923e20c5d9fbd34bebf5e11319b5d46e58e18
lonalee <42775674+lonalee@users.noreply.github.com>
2023-11-13 18:05:27 +0900

    share the entire dependencies

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,6 +2,7 @@ const {merge} = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
     mode: 'development',
@@ -19,7 +20,7 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap' 
                 // container에서 import문으로 접근하는 경로: 로드할 파일
             },
-            shared: ['react','react-dom']
+            shared: packageJson.dependencies
 
         }),
         new HtmlWebpackPlugin({
